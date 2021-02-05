afc18f6bceee5642792c27dafa2b72dcfb92f7e9
stolenlegos <mrizekm@gmail.com>
2021-02-05 10:01:46 -0500

    Commit First Project
    For assignment "Change something and check into GitHub". Added forward velocity and WASD rotaional controls.

--- Test Project/Assets/DropObject.cs
+++ Test Project/Assets/DropObject.cs
@@ -16,6 +16,23 @@ public class DropObject : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space)) {
           Rigidbody rb = gameObject.GetComponent<Rigidbody>();
           rb.useGravity = true;
+          rb.AddForce(transform.forward * 1000.0f);
+        }
+
+        if (Input.GetKey(KeyCode.W)) {
+          transform.Rotate(Vector3.right * 60 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.S)) {
+          transform.Rotate(Vector3.left * 60 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.A)) {
+          transform.Rotate(Vector3.down * 60 * Time.deltaTime);
+        }
+
+        if (Input.GetKey(KeyCode.D)) {
+          transform.Rotate(Vector3.up * 60 * Time.deltaTime);
         }
     }
 }
