bc770f3c3f3a5f579005be5ea6abb28c391694a7
stolenlegos <stolenlegos@gmail.com>
2021-02-26 15:48:23 -0500

    Vampire Initial Commit

---
+++ Vampire/Assets/Scripts/playerMovement.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+  public float moveSpeed;
+  public float lookSpeed;
+  private Rigidbody rb;
+  private float movementInput;
+  private float turnInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+      rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turnInput = Input.GetAxis("Horizontal");
+        movementInput = Input.GetAxis("Vertical");
+    }
+
+    void FixedUpdate()
+    {
+      Move();
+      Turn();
+    }
+
+    private void Turn()
+    {
+      float turn = turnInput * lookSpeed * Time.deltaTime;
+
+      Quaternion turnRotation = Quaternion.Euler(0, turn, 0);
+      rb.MoveRotation(rb.rotation * turnRotation);
+    }
+
+    private void Move()
+    {
+      Vector3 movement = transform.forward * movementInput * moveSpeed * Time.deltaTime;
+
+      rb.MovePosition(rb.position + movement);
+    }
+}
